30313bf8a7563f655ab7e31b3a19b9689eaded4f
bklcross <bklcross@gmail.com>
2022-08-10 20:24:56 -0700

    gh pages chages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, { useState } from 'react';
-// Here we import our Welcome component from /components
-import Welcome from './components/Welcome';
+// We import useState and useEffect in our component
+import React, { useState, useEffect } from 'react';
+import SearchBar from './components/SearchBar';
+import IssueList from './components/IssueList';
 
 function App() {
-  // We declare two state variables and functions to update both
-  // Name is the user's name, topic is what the user wants to learn
-  const [name, setName] = useState('');
-  const [topic, setTopic] = useState('');
+  // We declare a state variable that is an array called `issues` and a function to update it.
+  const [issues, setIssues] = useState([]);
+
+  // When the page loads, set the document title to something specific to this app.
+  // This only runs once because of our empty dependency array.
+  useEffect(() => {
+    document.title = 'GitHub issues';
+  }, []);
+
+  // Helper function that performs an API request and sets the `issues` array to a list of issues from GitHub
+  const getRepoIssues = (repo) => {
+    let issuesURL = `https://api.github.com/repos/${repo}/issues?direction=asc`;
+    console.log('issuesURL', issuesURL);
+    fetch(issuesURL)
+      .then((res) => res.json())
+      .then((response) => setIssues(response));
+  };
 
   return (
-    <div className="container">
-      <div>
-        <span>Enter your name: </span>
-        <input onChange={(e) => setName(e.target.value)} />
+    <div className="ui container">
+      {/* Here we pass our getRepoIssues function as a prop to SearchBar */}
+      <SearchBar onFormSubmit={getRepoIssues} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <IssueList issues={issues} />
+          </div>
+        </div>
       </div>
-      <div>
-        <span>What do you want to learn? </span>
-        <input onChange={(e) => setTopic(e.target.value)} />
-      </div>
-      <hr />
-      <Welcome name={name} topic={topic} />
     </div>
   );
 }
